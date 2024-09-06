5f9f5a67e849c8327b56ab2f24ad6548b23f5458
Vitor Onofre Ramos <107143879+VitorOnofreRamos@users.noreply.github.com>
2024-09-06 08:23:28 -0300

    Aula 06/09/2024

--- 2tdspc-primeira-api-master/src/main/java/br/com/fiap/primeira_api/controller/LivroController.java
+++ 2tdspc-primeira-api-master/src/main/java/br/com/fiap/primeira_api/controller/LivroController.java
@@ -7,6 +7,10 @@ import br.com.fiap.primeira_api.repository.LivroRepository;
 import br.com.fiap.primeira_api.service.LivroMapper;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 // localhost:8080/livros
@@ -23,14 +30,14 @@ public class LivroController {
     private LivroRepository livroRepository;
     @Autowired
     private LivroMapper livroMapper;
-    
-    Pageable paginacao = PageRequest.of(0, 2, Sort.by("título".descending()));
+
+    Pageable paginacao = PageRequest.of(0, 2, Sort.by("titulo").descending());
 
     // CRUD - Create, Read, Update, Delete
     // HTTP verbs - POST, GET, PUT, DELETE
     @PostMapping
     public ResponseEntity<LivroResponse> createLivro(@Valid @RequestBody LivroRequest livroRequest) {
-        Page<Livro> = listaLivros.requestToLivro(livroRequest);
+        Livro livroConvertido = livroMapper.requestToLivro(livroRequest);
         Livro livroCriado = livroRepository.save(livroConvertido);
         LivroResponse livroResponse = livroMapper.livroToResponse(livroCriado);
         return new ResponseEntity<>(livroResponse, HttpStatus.CREATED);
@@ -38,13 +45,19 @@ public class LivroController {
 
     @GetMapping
     public ResponseEntity<List<LivroResponse>> readLivros() {
-        List<Livro> listaLivros = livroRepository.findAll();
+        Page<Livro> listaLivros = livroRepository.findAll(paginacao);
         if (listaLivros.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         List<LivroResponse> listaLivrosResponse = new ArrayList<>();
         for (Livro livro : listaLivros) {
             LivroResponse livroResponse = livroMapper.livroToResponse(livro);
+            livroResponse.setLink(
+                    linkTo(
+                            methodOn(LivroController.class)
+                                    .readLivro(livroResponse.getId())
+                    ).withSelfRel()
+            );
             listaLivrosResponse.add(livroResponse);
         }
         return new ResponseEntity<>(listaLivrosResponse, HttpStatus.OK);
@@ -57,6 +70,12 @@ public class LivroController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         LivroResponse livroResponse = livroMapper.livroToResponse(livroSalvo.get());
+        livroResponse.setLink(
+                linkTo(
+                        methodOn(LivroController.class)
+                                .readLivros()
+                ).withRel("Lista de Livros")
+        );
         return new ResponseEntity<>(livroResponse, HttpStatus.OK);
     }
 
@@ -83,12 +102,3 @@ public class LivroController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
-
-
-
-
-
-
-
-
-
